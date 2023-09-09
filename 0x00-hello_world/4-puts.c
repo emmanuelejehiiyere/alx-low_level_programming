59c58834374ccdf24f1e95ab7ac7c252c18c7d2e
emmanuelejehiiyere <dasilemmanuel@gmail.com>
2023-09-09 13:27:04 -0700

    Hello, puts 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- the main method, where all the codes are checked and the entry point of code
+ * return- it will always return 0 is it is successful(erro free)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}

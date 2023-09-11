93c5afc99b6e13deddde9ee93cb045db6a4292f2
emmanuelejehiiyere <dasilemmanuel@gmail.com>
2023-09-11 15:46:46 -0700

    Numbers 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int emma;
+
+	for (emma = 0; emma < 10; emma++)
+	{
+		printf("%d", emma);
+	}
+	printf("\n");
+	return (0);
+}

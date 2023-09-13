57c6a7e789ddf1fd8748a6ab31a5832dd4357841
emmanuelejehiiyere <dasilemmanuel@gmail.com>
2023-09-12 19:41:52 -0700

    Function returned result of sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - Function for task
+ *
+ *NB: it sums to integers and display the results
+ * @num1: first integer
+ * @num2: second integer
+ *
+ * Return:sum.
+ */
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}

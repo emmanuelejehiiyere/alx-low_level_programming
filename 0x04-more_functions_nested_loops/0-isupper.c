2aaf8e5faab346efc93673e0b78887505fcf4636
emmanuelejehiiyere <dasilemmanuel@gmail.com>
2023-09-14 11:51:30 -0700

    Checked if c is upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - Function for task
+ *@c: Check for upperCase
+ *Return: 1 if upper case
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}

aea1832f56e08b50d313a7d0bcab82a765579d12
emmanuelejehiiyere <dasilemmanuel@gmail.com>
2023-09-14 12:14:12 -0700

    Multiplied two values

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Function forr task
+ * @a: Variable (type int)
+ * @b: Variable (type int)
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}

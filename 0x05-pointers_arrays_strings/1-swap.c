5bcb9441b91bfe23f39be9cd7e656ad800af2fdd
emmanuelejehiiyere <dasilemmanuel@gmail.com>
2023-09-18 13:21:51 -0700

    Swapped the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * swap_int - Function for tak
+ * @a: Variable type int
+ * @b: Variable type int
+ * Return: void (nothing)
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}

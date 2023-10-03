e160bbc666ceda0d3b03786342e3c9e65803c29c
emmanuelejehiiyere <dasilemmanuel@gmail.com>
2023-10-03 05:45:38 -0700

    Printed he minimum number of coins

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+	 * check_num - check - string there are digit
+	 * @str: An array type string
+	 * Return: Always 0 (Successful)
+	 */
+	int check_num(char *str)
+	{
+		unsigned int i;
+
+		i = 0;
+		while (i < strlen(str))
+		{
+			if (!isdigit(str[i]))
+			{
+				return (0);
+			}
+			i++;
+		}
+		return (1);
+	}
+
+	/**
+	 * main - Displays the name of the program
+	 * @argc: Count argument
+	 * @argv: Arguments
+	 * Return: Always 0 (Successful)
+	 */
+
+	int main(int argc, char *argv[])
+	{
+		int i;
+		int str_to_int;
+		int sum = 0;
+
+		i = 1;
+		while (i < argc)
+		{
+			if (check_num(argv[i]))
+			{
+				str_to_int = _atoi(argv[i]);
+				sum = sum + str_to_int;
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+			i++;
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 int _putchar(char c);
 int _atoi(char *s);

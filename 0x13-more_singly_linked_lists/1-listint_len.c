558fdcde2b41457263277255605d395f7852ad3b
emmanuelejehiiyere <dasilemmanuel@gmail.com>
2023-10-24 07:50:13 -0700

    Wrote a func that returns num of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * listint_len - returns the num of elements in a linked listint_t list
+ * @h: linked list of type listint_t parameter
+ * Return: num of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t number = 0;
+	while (h)
+	{
+		number++;
+		h = h->next;
+	}
+	return (number);
+}

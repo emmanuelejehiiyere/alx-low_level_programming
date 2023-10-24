a03d5710ccc519548c89dd3a0fde564242002586
emmanuelejehiiyere <dasilemmanuel@gmail.com>
2023-10-24 07:39:20 -0700

    Wrote a func that prints all elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: linked list of type listint_t to print
@@ -7,14 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
- size_t number = 0;
+	size_t number = 0;
 
- while (h)
- {
- printf("%d\n", h->n);
- number++;
- h = h->next;
- }
-
- return (number);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		number++;
+		h = h->next;
+	}
+	return (number);
 }

5ff9db614bf9475ad5834f61cfe4b9b10ba8ff4f
emmanuelejehiiyere <dasilemmanuel@gmail.com>
2023-10-25 07:36:41 -0700

    Wrote a func that fress listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
+ * Return: void (nothing)
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tp;
+
+	if (head == NULL)
+		return;
+	while (*head)
+	{
+		tp = (*head)->next;
+		free(*head);
+		*head = tp;
+	}
+	*head = NULL;
+}
